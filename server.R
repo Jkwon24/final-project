6e7c3b6bef30c3f8d487cbc6f4e67a3609756cb7
Conner-JW <connerjw@uw.edu>
2018-03-05 21:51:22 -0800

    Conners Branch. Functional Name and Move search. Superficial type selection drop down menu completed.

--- server.R
+++ server.R
@@ -1,50 +1,69 @@
 # Final Project Server File
 
 source("PokemonMap.R")
-
+library("dplyr")
 my.server <- function(input, output) {
   
-  #--------------Text Box Reactives In Here--------------#
-  test.reactive <- reactive({
+  pokemonInfo <- read.csv("data/PokemonInfo.csv")
+  moveInfo <- read.csv("data/PokemonAbilities.csv")
+  
+  
+  
+  #--------------Pokemon Names Reactives In Here--------------#
+  pokemonName.reactive <- reactive({
     return(input$PokemonName)
   })
   
   # Reactive Pokemon Name for Text Input
   output$pokemon <- renderText({
-    return(test.reactive())
+    return(pokemonName.reactive())
   })
+  #---------------Make a mini-table of data--------------#
+  #table1 <- filter(pokemonInfo, "Name" == pokemonName.reactive)
   
+  output$filteredTable <- renderTable({
+    #test.table <- pokemonInfo
+    newTable <- filter(pokemonInfo, pokemonInfo[, 3] == pokemonName.reactive())
+  })
   #------------------------------------------------------#
-  
-  
-  
-  
-  #---------------Gen Reactives In Here------------------#
-  # Reactive Generation Variable for Table 1
-  generatoin.reactive <- reactive({
-    return(input$Generation)
-  })
-  
-  output$generation <- renderTable({
-    # CALL 'GET' on a specific URL based on Reactive Gen data
-  })
-  
-  
-  #------------------------------------------------------#
-  
-  
-  
-  
-  # Reactive input for slider, adds "yr" to begining to help filter with DPLYR
-  slider.reactive <- reactive({
-    filter.value <- input$SliderYear
-    filter.value.string <- paste0("YR", filter.value)
-    return(filter.value.string)
+
+  output$pokemonImage <- renderText({
+    target <- pokemonInfo[1 == pokemonName.reactive(), 4]
+    
+    
   })
   
  
+  
+  #--------------Move Name Reactives In Here--------------#
+  moveName.reactive <- reactive({
+    return(input$MoveName)
+  })
+  
+  # Reactive Pokemon Name for Text Input
+  output$move <- renderText({
+    return(moveName.reactive)
+  })
+  #---------------Make a mini-table of data--------------#
+  output$moveTable <- renderTable({
+    moveTable <- filter(moveInfo, moveInfo[, 4] == moveName.reactive())
+  })
+  #------------------------------------------------------#
+  
+  
+  typeName.reactive <-reactive({
+    return
+    
+  })
+  
+  
+  
+  
+  
+  
   output$pokemon.map <- renderLeaflet({
     return(kanto.region)
   })
   
 }  
+
--- ui.R
+++ ui.R
@@ -1,81 +1,86 @@
 # Final Project UI Code
+
+
 library("shinythemes")
+library("ggplot2")
+library("leaflet")
+
+pokemonInfo <- read.csv("data/PokemonInfo.csv")
 
 my.ui <- fluidPage(theme = shinytheme("darkly"),
-  tags$head(
-    tags$style(HTML(".leaflet-container { background: #ffffff; }"))
-  ),
-  titlePanel("Pokemon Data"),
-  
-  tabsetPanel(
-    # TABLE TABS 
-    tabPanel("Table 1 [Conner]",
-             sidebarLayout(
-               # Sidebar Widgets
-               sidebarPanel(
-                 h2("Table Interactivity Tools"),
-                 
-                 # Use the slider for something (only show a generation, or X amount of values)
-                 sliderInput("SliderValue", "Slider [Potential Widget",
-                             min = 0, # THESE VALUES ARE ARBITRARY 
-                             max = 50,
-                             value = 0
-                 ),
-                 
-                 # Perhaps you can search for a specific pokemon
-                 textInput("PokemonName", "Text Input Widget"),
-                 
-                 #ADD MORE WIDGETS HERE ONCE YOU HAVE THE TABLE MADE
-                 
-                 selectInput("Generation","Please Select Generation to Parse [Conner]",
-                             c("Gen1",
-                               "Gen2",
-                               "Gen3",
-                               "Gen4",
-                               "Gen5",
-                               "Gen6"
-                             ))
-               ),
-               
-               
-               #-------------------- ^ Sidebar Panel Rendering Goes Above  ^ ---------------------#
-               #--------------------                                         ---------------------#
-               #-------------------- V Main Panel Rendering Goe Below Here V ---------------------#
-               
-               
-               mainPanel(
-                 h2("Pokemon Generation Data")
-               ) 
-             ),
-             
-             # Lets me show them what they are typing in inside my panel + makes reactive
-             textOutput("pokemon")
-             # Goal: Let User input
-    ),
-    
-    
-    
-    
-    tabPanel("Table 2 [Josh]"
-             
-    ),
-    
-    
-    
-    
-    
-    tabPanel("Table or Map 3 [Karan]"
-             
-    ),
-    
-    
-    
-    
-    
-    tabPanel("Kanto Map",
-             leafletOutput("pokemon.map")
-    )
-  )
+                   tags$head(
+                     tags$style(HTML(".leaflet-container { background: #ffffff; }"))
+                   ),
+                   titlePanel("Pokemon Data"),
+                   
+                   tabsetPanel(
+                     # TABLE TABS 
+                     tabPanel("Table 1 [Conner]",
+                              sidebarLayout(
+                                # Sidebar Widgets
+                                sidebarPanel(
+                                  h2("Pokemon Search!"),
+                                  textInput("PokemonName", "Please Enter a Pokemon Name"),
+                                  h2("Move Search"),
+                                  textInput("MoveName", "Please Enter a Move Name"),
+                                  h2("Filter by Pokemon Type"),
+                                  selectInput("typeName", "Type of Pokemon", 
+                                              c("fire", "water", "poision", "grass", "flying", 
+                                                "bug", "normal", "electric", "ground", "fairy",
+                                                "grass", "fighting", "psychic", "rock", "Steel",
+                                                "ice", "dragon")
+                                              )
+                                              
+                                           
+                                ),
+                                
+                                
+                                #-------------------- ^ Sidebar Panel Rendering Goes Above  ^ ---------------------#
+                                #--------------------                                         ---------------------#
+                                #-------------------- V Main Panel Rendering Goe Below Here V ---------------------#
+                                
+                                
+                                mainPanel(
+                                  h2("Pokemon Database Information Panel"),
+                                  
+                                  # Lets me show them what they are typing in inside my panel + makes reactive
+                                  h3("Showing information on:"),
+                                  chosenPokemon <- textOutput("pokemon"),
+                                  #print(pokemon.NameReactive)
+                                  tableOutput("filteredTable"),
+                                  tableOutput("moveTable"),
+                                  
+                                  htmlOutput("pokemonImage")
+                                  
+                                  
+                                  
+                                ) 
+                              )
+                     ),
+                     
+                     
+                     
+                     
+                     tabPanel("Table 2 [Josh]"
+                              
+                     ),
+                     
+                     
+                     
+                     
+                     
+                     tabPanel("Table or Map 3 [Karan]"
+                              
+                     ),
+                     
+                     
+                     
+                     
+                     
+                     tabPanel("Kanto Map",
+                              leafletOutput("pokemon.map")
+                     )
+                   )
 ) 
 
 
