7acb43a852a39e04d4eb0c42c9d52b1397bca4de
Joshua <joshuakwon24@yahoo.com>
2018-03-08 10:22:46 -0800

    Final commit, fixed and touched up on map

--- server.R
+++ server.R
@@ -300,10 +300,10 @@ my.server <- function(input, output) {
   })
   
   output$pokemon.map.one.shade <- renderPlot({
-    kanto.region <- ggplot() +
+    kanto.region <- ggplot() + 
       geom_polygon(data = map.plots, aes(x = x, y = y, group = group, fill = location_type)) +
       geom_polygon(data = plot.list.of.location(), aes(x = x, y = y, group = group, fill = location_name)) + 
-      scale_fill_manual(values = c("#e6194b", , "#0082c8", "#3cb44b", "#FFFF00", "#FFFF00", "#FFFF00", "#FFFF00"
+      scale_fill_manual(values = c(  "#e6194b", "#3cb44b", "#0082c8", "#FFFF00", "#FFFF00", "#FFFF00", "#FFFF00"
                                    , "#FFFF00", "#FFFF00", "#FFFF00", "#FFFF00", "#FFFF00", "#FFFF00", "#FFFF00"
                                    , "#FFFF00", "#FFFF00", "#FFFF00", "#FFFF00", "#FFFF00", "#FFFF00", "#FFFF00"
                                    , "#FFFF00", "#FFFF00", "#FFFF00")) +
@@ -320,11 +320,11 @@ my.server <- function(input, output) {
     return(kanto.region)
   })
   
-  output$pokemon.map <- renderPlot({
+  output$pokemon.map <- renderPlot({ 
     kanto.region <- ggplot() +
       geom_polygon(data = map.plots, aes(x = x, y = y, group = group, fill = location_type)) +
       geom_polygon(data = plot.list.of.location(), aes(x = x, y = y, group = group, fill = location_name)) + 
-      scale_fill_manual(values = c("#e6194b", "#0082c8", "#3cb44b", "#FFFF00", "#FF00FF", "#C0C0C0", "#f58231", "#00FF00"
+      scale_fill_manual(values = c(  "#e6194b", "#3cb44b", "#0082c8", "#FFFF00", "#FF00FF", "#C0C0C0", "#f58231", "#00FF00"
                                    , "#911eb4", "#87CEFA", "#46f0f0", "#000000", "#808080", "#6A5ACD", "#d2f53c", "#fabebe"
                                    , "#008080", "#e6beff", "#aa6e28", "#fffac8", "#800000", "#aaffc3", "#808000", "#ffd8b1")) +
       coord_quickmap() +
