1786710fe5207045869cffcef5189201f7988ed4
Karan Choksi <karanchoksi96@gmail.com>
2018-03-08 10:48:06 -0800

    Fixed final problems with color on map

--- server.R
+++ server.R
@@ -303,11 +303,8 @@ my.server <- function(input, output) {
     kanto.region <- ggplot() + 
       geom_polygon(data = map.plots, aes(x = x, y = y, group = group, fill = location_type)) +
       geom_polygon(data = plot.list.of.location(), aes(x = x, y = y, group = group, fill = location_name)) + 
-<<<<<<< HEAD
-      scale_fill_manual(values = c(  "#e6194b", "#3cb44b", "#0082c8", "#FFFF00", "#FFFF00", "#FFFF00", "#FFFF00"
-=======
-      scale_fill_manual(values = c("#e6194b", "#0082c8", "#3cb44b", "#FFFF00", "#FFFF00", "#FFFF00", "#FFFF00"
->>>>>>> c84f3ea7d56158284f7ecc2623fabc026133169d
+
+      scale_fill_manual(values = c("#e6194b", "#3cb44b", "#0082c8", "#FFFF00", "#FFFF00", "#FFFF00", "#FFFF00"
                                    , "#FFFF00", "#FFFF00", "#FFFF00", "#FFFF00", "#FFFF00", "#FFFF00", "#FFFF00"
                                    , "#FFFF00", "#FFFF00", "#FFFF00", "#FFFF00", "#FFFF00", "#FFFF00", "#FFFF00"
                                    , "#FFFF00", "#FFFF00", "#FFFF00")) +
